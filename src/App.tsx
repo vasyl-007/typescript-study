c7fdbfa3a6e9b5ea7d42ae14eeb3e474b8e94cca
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-18 00:31:56 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const addHandler = (title: string) => {
-    console.log("Add new Todo", title);
+    // console.log("Add new Todo", title);
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    };
+    // setTodos([newTodo, ...todos]);
+    setTodos(prev => [newTodo, ...prev])
   };
 
   return (
@@ -14,6 +23,7 @@ const App: React.FC = () => {
       <Navbar />
       <div className="container">
         <TodoForm onAdd={addHandler} />
+        <TodoList todos={todos} />
       </div>
     </>
   );
